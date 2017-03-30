314596ba23e9397a2d7ac0c865965f43fa4cbae6
Siddharth G <cdart.dav@gmail.com>
2017-03-30 13:17:55 +0530

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -483,7 +483,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var AF = (function () {
     function AF(af) {
         this.af = af;
-        console.log("trying now");
         this.messages = this.af.database.list('messages');
         this.users = this.af.database.list('users');
     }
@@ -495,7 +494,7 @@ var AF = (function () {
     AF.prototype.loginWithGoogle = function () {
         return this.af.auth.login({
             provider: __WEBPACK_IMPORTED_MODULE_1_angularfire2__["b" /* AuthProviders */].Google,
-            method: __WEBPACK_IMPORTED_MODULE_1_angularfire2__["c" /* AuthMethods */].Popup
+            method: __WEBPACK_IMPORTED_MODULE_1_angularfire2__["c" /* AuthMethods */].Redirect
         }).catch(function (error) {
             console.log(error);
         });
